dd4a7b3c8062c8eeb3c8cef65248e837c94242cf
UserAkku <akhileshkumaroffical@gmail.com>
2025-01-29 01:28:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+gsap.to("#nav",{
+    backgroundColor: "#000",
+    height:"120px",
+    duration:0.5,
+    scrollTrigger:{
+       trigger:"#nav",
+       scroller:"body",
+       markers:true,
+       start:"top -10%  "
+    }
+} )
